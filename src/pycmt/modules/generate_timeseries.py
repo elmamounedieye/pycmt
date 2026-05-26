9fa42c92c9777915d6e6991b0bf80faf63961875
Mouhamed El Mamoune DIEYE <memdieye@aimsammi.org>
2026-05-26 14:00:25 -0400

    paths changing

--- src/pycmt/modules/generate_timeseries.py
+++ src/pycmt/modules/generate_timeseries.py
@@ -124,12 +124,12 @@ def generate_tseries(country_iso, country, rndta):
     #clim_data = open_CtlDataset(os.path.relpath(clim_path))
     print(f"daily precip path: {daily_precip_path}")
     print(f"==================================")
-    print(f"daily precip path with Path: {Path(daily_precip_path)}")
+    print(f"daily precip path with Path: {Path(daily_precip_path).resolve()}")
     print(f"==================================")
-    print(f"daily precip path with Path: {str(Path(daily_precip_path))}")
+    print(f"daily precip path with Path: {str(Path(daily_precip_path).resolve())}")
     print(f"==================================")
-    daily_data = open_CtlDataset(Path(daily_precip_path).resolve())
-    clim_data = open_CtlDataset(Path(clim_path).resolve())
+    daily_data = open_CtlDataset(Path(daily_precip_path))
+    clim_data = open_CtlDataset(Path(clim_path))
 
     #######added######
 
